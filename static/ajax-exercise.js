bab6b29f9b1ced4d2df4fc37d4db4d5707c190b3
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-27 13:00:44 -0700

    finished showWeather()

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -14,14 +14,19 @@ $('#get-fortune-button').on('click', showFortune);
 
 // PART 2: SHOW WEATHER
 
+
 function showWeather(evt) {
     evt.preventDefault();
 
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
-
-
+    // "zipcode"  is set to same key as request.args.get is searching for
+    // the above is an object
     // TODO: request weather with that URL and show the forecast in #weather-info
+
+    $.get(url, formData, function(result) {
+    	$('#weather-info').html(`Your forecast: ${result['forecast']}`);
+    })
 }
 
 $("#weather-form").on('submit', showWeather);
